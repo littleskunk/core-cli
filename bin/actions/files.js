10fbcf7396149175afe074205b19d6f615864269
littleskunk <jens.heimbuerge@googlemail.com>
2016-11-25 23:27:53 +0100

    Update files.js

--- bin/actions/files.js
+++ bin/actions/files.js
@@ -210,10 +210,6 @@ module.exports.getallpointers = function(bucket, env) {
   bucket = this._storj.getRealBucketId(bucket);
   
   var whitelist = new Whitelist(path.join(HOME, '.storjcli'));
-  whitelist.push('3d2422e7d1a2355208cb0fdabd6ff31ce2f9d469');
-  return log('info', JSON.stringify(whitelist.toObject()));
-  
-  return log('info', 'Hack the planet');
   
   client.listFilesInBucket(bucket, function(err, files) {
     if (err) {
