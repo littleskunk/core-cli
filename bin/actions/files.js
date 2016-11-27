92710400faaa42d36f8cdfe3e55f719326694996
littleskunk <jens.heimbuerge@googlemail.com>
2016-11-27 11:42:14 +0100

    Update files.js

--- bin/actions/files.js
+++ bin/actions/files.js
@@ -275,10 +275,6 @@ module.exports.getallpointers = function(bucket, env) {
       }
       
       _getFilePointers(file, 0);
-      _getFilePointers(file, 0);
-      _getFilePointers(file, 0);
-      _getFilePointers(file, 0);
-      _getFilePointers(file, 0);
       return log('info', 'Hack the Planet');
     });
   });
