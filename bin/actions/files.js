3ac3316692d54bc326ed4adb605c0f60f332db97
littleskunk <jens.heimbuerge@googlemail.com>
2016-11-28 14:42:14 +0100

    Update files.js

--- bin/actions/files.js
+++ bin/actions/files.js
@@ -218,7 +218,7 @@ module.exports.getallpointers = function(bucket, env) {
   var filelist = JSON.parse(fs.readFileSync(path.join(HOME, '.storjcli/.files')));
   var whitelist = new Whitelist(path.join(HOME, '.storjcli'));
     
-  async.forEachLimit(filelist, 10, function(file, callback) {
+  async.forEachLimit(filelist, 100, function(file, callback) {
     client.createToken(file.bucket, 'PULL', function(err, token) {
       if (err) {
         log('warn', 'Create Token: %s', err.message);
